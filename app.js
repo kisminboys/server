4b360813d5007d941697e9a31c9e0ad9549390a1
reyhanprtrs <reyhanprtrs@gmail.com>
2020-12-21 14:51:20 +0700

    put errorhandler in app

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const MainRouter = require('./routers')
+const errorHandler = require('./middlewares')
 
 app.use(cors())
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
 app.use('/', MainRouter)
+app.use(errorHandler)
 
 module.exports = app;

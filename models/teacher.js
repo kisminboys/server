3d20c761ed0550c3f48f22756176318670947524
RizkyMaulita <rizky.maulita@gmail.com>
2020-12-21 16:26:55 +0700

    add validate in teacher

--- models/teacher.js
+++ models/teacher.js
@@ -11,20 +11,93 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Teacher.hasMany(models.Classroom)
+      Teacher.belongsTo(models.Subject)      
     }
   };
   Teacher.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    fullName: DataTypes.STRING,
-    address: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    photo: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    birthDate: DataTypes.DATEONLY,
-    password: DataTypes.STRING,
-    ClassroomId: DataTypes.INTEGER,
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `First Name can't be empty`},
+        notNull: { msg: `First Name can't be empty`}
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Last Name can't be empty`},
+        notNull: { msg: `Last Name can't be empty`}
+      }
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Full Name can't be empty`},
+        notNull: { msg: `Full Name can't be empty`}
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Address can't be empty`},
+        notNull: { msg: `Address can't be empty`}
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `E'mail can't be empty`},
+        notNull: { msg: `E'mail can't be empty`},
+        isEmail: { msg: `E'mail must be formatted in example@mail.com`}
+      }
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Phone number can't be empty`},
+        notNull: { msg: `Phone number can't be empty`},
+      }
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Photo can't be empty`},
+        notNull: { msg: `Photo can't be empty`},
+      }
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Gender can't be empty`},
+        notNull: { msg: `Gender can't be empty`},
+      }
+    },
+    birthDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Birth Date can't be empty`},
+        notNull: { msg: `Birth Date can't be empty`},
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: `Password can't be empty`},
+        notNull: { msg: `Password can't be empty`},
+        len: { args: [7], msg: `Password must be contain minimum 7 characters`}
+      }
+    },
     SubjectId: DataTypes.INTEGER
   }, {
     sequelize,

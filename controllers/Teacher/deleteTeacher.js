2c2d363a135d6821b7e8a3e9877dc03faaf763cf
tiorzal <tioralmunazir@gmail.com>
2020-12-22 11:50:58 +0700

    update routing teacher
    rapihin route teacher

--- controllers/Teacher/deleteTeacher.js
+++ controllers/Teacher/deleteTeacher.js
@@ -1,7 +1,7 @@
 const { Teacher } = require('../../models')
 
 module.exports = async (req, res, next) => {
-  const id = req.loginUser.id
+  const id = req.params.id
 
   try {
     const findTeacher = await Teacher.findByPk(id)
@@ -10,6 +10,7 @@ module.exports = async (req, res, next) => {
       const deleteTeacher = await Teacher.destroy({
         where: { id } 
       })
+      res.status(200).json({ message: 'teacher delete'})
     }else throw { status: 404, message: 'teacher not found, cant be deleted'}
   } catch (error) {
     next(error)
--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const AdminRouter = require('./AdminRouter')
 const StudentRouter = require('./StudentRouter')
+const TeacherRouter = require('./TeacherRouter')
 
 router.use('/admin', AdminRouter)
 router.use('/student', StudentRouter)
+router.use('/teacher', TeacherRouter)
 
 module.exports = router

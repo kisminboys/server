2f889e80159f7801a953f6d4ca8f8ff9b5a91e09
RizkyMaulita <rizky.maulita@gmail.com>
2020-12-22 10:32:18 +0700

    add associate in question bank

--- models/questionbank.js
+++ models/questionbank.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      QuestionBank.belongsTo(models.Subject)
+      QuestionBank.hasMany(models.Exam)
     }
   };
   QuestionBank.init({

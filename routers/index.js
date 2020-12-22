3edb301491f18392752385acffbdab0b1026c635
tiorzal <tioralmunazir@gmail.com>
2020-12-22 14:47:53 +0700

    update login
    pasang di router
    maap kelupaan, jgn di marahin, makasih

--- routers/index.js
+++ routers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 const AdminRouter = require('./AdminRouter')
 const StudentRouter = require('./StudentRouter')
 const TeacherRouter = require('./TeacherRouter')
+const { Login } = require('../controllers')
 
 router.use('/admin', AdminRouter)
+router.post('/login', Login)
 router.use('/student', StudentRouter)
 router.use('/teacher', TeacherRouter)
 

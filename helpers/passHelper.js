c748a8e18a432c01aa1c7c9bf34b70e213d6d02d
tiorzal <tioralmunazir@gmail.com>
2020-12-23 11:35:58 +0700

    update random password

--- helpers/passHelper.js
+++ helpers/passHelper.js
@@ -9,6 +9,16 @@ class PassHelper {
   static comparePassword (plain, hash) {
     return bcrypt.compareSync(plain, hash)
   }
+
+  static randomPassword () {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < 10; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 }
 
 module.exports = PassHelper
--- routers/StudentRouter.js
+++ routers/StudentRouter.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const { Student } = require("../controllers");
+const { authorizationAdmin } = require("../middlewares")
 
-router.get('/', Student.getAllStudent)
-// router.post('/create', Student.createAccount)
-router.put('/reset', Student.resetPassword)
 router.get('/:id', Student.findStudentById)
+router.put('/', Student.editAccount)
+router.patch('/', Student.resetPassword)
+
+router.use(authorizationAdmin)
+router.get('/', Student.getAllStudent)
+router.post('/', Student.createAccount)
+router.delete('/:id', Student.deleteAccount)
 
 module.exports = router;
